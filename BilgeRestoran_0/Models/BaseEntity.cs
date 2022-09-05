66fad720d625021e254ce7b1ae6ea2c24ca0d23b
sefer ersan aydin <ersanaydin@outlook.com>
2022-09-05 11:39:01 +0300

    ek

--- BilgeRestoran_0/Models/BaseEntity.cs
+++ BilgeRestoran_0/Models/BaseEntity.cs
@@ -14,10 +14,16 @@ namespace BilgeRestoran_0.Models
         {
             get
             {
+                if (_fiyat <= 0)
+                {
+                    System.Windows.Forms.MessageBox.Show("Dikkat ediniz ürün fiyatı yanlış girilmiştir, sonuc 0 olacaktır");
+                    throw new Exception("ürün fiyatı 0");
+                }
                 return _fiyat;
             }
             set
             {
+                if (value <= 0) throw new Exception("Fiyat 0 veya 0 dan küçük olamaz");
                 _fiyat = value;
             }
         }
